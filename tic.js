7eff0ad7499c11781f3734a96960ff3da040b324
Prashant Raghav <prashantraghavp293@gmail.com>
2024-10-02 21:15:25 +0530

    Add files via upload
    Initial upload of Tic-Tac-Toe game files

---
+++ tic.js
@@ -0,0 +1,92 @@
+const cells = document.querySelectorAll('[data-cell]');
+const board = document.querySelector('.game-board');
+const winningMessageElement = document.getElementById('winningMessage');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const restartButton = document.getElementById('restartButton');
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+let oTurn;
+
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+  oTurn = false;
+  cells.forEach(cell => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(O_CLASS);
+    cell.textContent = '';  // Clear the content
+    cell.removeEventListener('click', handleClick);
+    cell.addEventListener('click', handleClick, { once: true });
+  });
+  setBoardHoverClass();
+  winningMessageElement.style.display = 'none';
+}
+
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = oTurn ? O_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = "Draw!";
+  } else {
+    winningMessageTextElement.innerText = `${oTurn ? "O" : "X"} Wins!`;
+  }
+  winningMessageElement.style.display = 'block';
+}
+
+function isDraw() {
+  return [...cells].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+  });
+}
+
+function placeMark(cell, currentClass) {
+  cell.textContent = currentClass === X_CLASS ? 'X' : 'O';  // Add X or O to the cell
+  cell.classList.add(currentClass);  // Apply the color class
+}
+
+function swapTurns() {
+  oTurn = !oTurn;
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS);
+  board.classList.remove(O_CLASS);
+  if (oTurn) {
+    board.classList.add(O_CLASS);
+  } else {
+    board.classList.add(X_CLASS);
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cells[index].classList.contains(currentClass);
+    });
+  });
+}
